0e99caca58b6f0dee4638c2d9ef45b8b9760401c
Paulo Henrique <phas2010@gmail.com>
2016-08-19 23:25:31 -0300

    Atualização CelsiusoFahrenheitConverter

--- jsf-teste/src/main/java/lab3/CelsiusToFahrenheitConverter.java
+++ jsf-teste/src/main/java/lab3/CelsiusToFahrenheitConverter.java
@@ -9,26 +9,32 @@ import javax.faces.convert.FacesConverter;
 
 @FacesConverter("celsiusTofahrenheitConverter")
 public class CelsiusToFahrenheitConverter implements Converter {
-	//,@Override
 	public Object getAsObject(FacesContext context, UIComponent component, String value) {
+
 		float result = 0F;
+
 		try {
 			float celsius = Float.parseFloat(value);
-			result = (celsius * 9/5) + 32;
+
+			result = (celsius * (9 / 5) + 32);
+
 		} catch (Exception e) {
-			FacesMessage msg= new FacesMessage("Erro na conversão de celsius para fahreinheit","Entrada Invalida, Tente Novamente");
+
+			FacesMessage msg = new FacesMessage("Erro na conversão de celsius para fahreinheit",
+					"Entrada Invalida, Tente Novamente");
 			msg.setSeverity(FacesMessage.SEVERITY_ERROR);
-			
+
 			throw new ConverterException(msg);
 		}
+
 		return result;
 	}
-	//@Override
-	public String getAsString (FacesContext context, UIComponent component, String value){
+
+	public String getAsString(FacesContext context, UIComponent component, String value) {
 		return value.toString();
 	}
-	public String getAsString(FacesContext arg0, UIComponent arg1, Object arg2) {
-		// TODO Auto-generated method stub
+
+	public String getAsString(FacesContext context, UIComponent component, Object value) {
 		return null;
 	}
 }
